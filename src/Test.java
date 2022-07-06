3e172bab17c1749baec7b65289787324aadcf2d6
siroba2 <silvia.rodriguez@scopely.com>
2022-07-06 16:54:11 +0200

    Added library fro complex numbers
    FFT function is not working correctly --> index out of bounds

--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,20 @@
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Test {
 
     public static void main(String args[]){
 
-        Fingerprint File = new Fingerprint("TestAudio/Kate Bush - Running Up That Hill (Song Lyrics).wav",0);
+        Fingerprint audio = new Fingerprint("TestAudio/Kate Bush - Running Up That Hill (Song Lyrics).wav",0);
         try {
-            File.toByteArray();
+            audio.toByteArray();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        double [] resultFFT= audio.FFT();
+        System.out.println(Arrays.toString(resultFFT));
+
 
     }
 }
